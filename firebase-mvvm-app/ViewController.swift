0076477203ddae72598592575c9f99f9106a97f4
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-03-26 15:21:36 +0900

    Initial commit

---
+++ firebase-mvvm-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  
+//  
+//  Created by Seigetsu on 2024/03/26
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
